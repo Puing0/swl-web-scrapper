045ac34011913a8e3722f6fef213238a3cc2b731
Puing0 <anfel.sara@yahoo.com>
2023-07-02 01:58:11 +0200

    last update

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,6 @@
 import requests
-import io
-from PIL import Image
+# import io
+# from PIL import Image
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,7 +14,7 @@ def get_images_url(wd, delay):
 
     scroll_down(wd)
 
-    images = wd.find_elements(By.CSS_SELECTOR, '[alt="HMIIF"]')
+    images = wd.find_elements(By.CSS_SELECTOR, '[alt="HMIBC"]')
     labels = wd.find_elements(By.CLASS_NAME, 'region_mag')
 
     image_urls = []
@@ -43,20 +43,20 @@ def download_image(download_path, url, file_name):
         with open(file_path, "wb") as file:
             file.write(response.content)
 
-        print("Image downloaded successfully:", file_path)
+        # print("Image downloaded successfully:", file_path)
     except Exception as e:
         print('FAILED -', e)
 
 def navigate_to_next_page(wd):
     anchor_point = wd.find_elements(By.XPATH, "//a[.//span[text()='Jour prochain']]")[0]
     wd.execute_script("arguments[0].click();", anchor_point) 
-    # anchor_point.click()
+    print(wd.current_url)
 
 # Set up the WebDriver
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Open the initial URL
-url = "https://www.spaceweatherlive.com/fr/archives/2023/05/01/dayobs.html"
+url = "https://www.spaceweatherlive.com/fr/archives/2022/01/01/dayobs.html"
 wd.get(url)
 
 # Variables for image count and delay
@@ -64,19 +64,29 @@ j = 0
 delay = 2
 
 # Main loop to navigate, extract image URLs, and download images
+alpha_idx = 0
+beta_idx = 0
+betax_idx = 0
 while j < 3000:
     try:
         image_urls, labels = get_images_url(wd, delay)
-
+        # print("image size url and labels: "+ len(image_urls)+" "+ len(labels))
         for i, url in enumerate(image_urls):
             label = labels[i]
+            yy = url[49:53]
+            mm = url[54:56]
+            dd = url[57:59]
+            date  = yy+"-"+mm+"-"+dd
 
             if label == 'A':
-                download_image("imgs/alpha/", url, str(j) + ".jpg")
+                download_image("imgs/alpha/", url, date+"_"+str(alpha_idx) + ".jpg")
+                alpha_idx+=1
             elif label == 'B':
-                download_image("imgs/beta/", url, str(j) + ".jpg")
+                download_image("imgs/beta/", url, date+"_"+str(beta_idx) + ".jpg")
+                beta_idx+=1
             else:
-                download_image("imgs/betax/", url, str(j) + ".jpg")
+                download_image("imgs/betax/", url, date+"_"+str(betax_idx) + ".jpg")
+                betax_idx+=1
             
             j += 1
 
